d3cf8ad6d6a825936fd5f51d0f317bc3ab563100
Rich McGhee <RichMcGhee@gmail.com>
2026-08-18 10:50:17 -0500

    Set version

--- src/parser.c
+++ src/parser.c
@@ -130006,8 +130006,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_python(void) {
     .reserved_words = &ts_reserved_words[0][0],
     .max_reserved_word_set_size = 35,
     .metadata = {
-      .major_version = 0,
-      .minor_version = 25,
+      .major_version = 1,
+      .minor_version = 0,
       .patch_version = 0,
     },
   };
